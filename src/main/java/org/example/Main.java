77c412ef5b0a47297060958592bd4f3dddaf27a7
binch95 <qlscm95@gmail.com>
2024-06-04 16:47:32 +0900

    work_7

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,37 +1,28 @@
 package org.example;
 
+import java.util.Arrays;
+
 class Main {
-    static int a = 10;
-
-    public static int 더하기(int a, int b) {
-        return a + b;
-    }
-
     public static void main(String[] args) {
+        boolean rs1 = 계산기.is_a_bigger_than_b(10, 20);
+        System.out.println(rs1);
+        rs1 = 계산기.is_a_bigger_than_b(240, 20);
+        System.out.println(rs1);
+        rs1 = 계산기.is_a_bigger_than_b(20, 20);
+        System.out.println(rs1);
 
-        System.out.println(a);
-         int a = 20;
-        System.out.println(a);
-
-        계산기.합(20,30);
-        계산기.합(20);
-        System.out.println(더하기(30,20));
-        exam();
-
-    }
-    public static void exam(){
-        System.out.println("a : " + a);
     }
 }
 
 class 계산기 {
 
-    static void 합(int c) {
-        System.out.println("c = " + c);
-    }
-
-    static void 합(int a, int b) {
-        System.out.println("a = " + a);
-        System.out.println("b = " + b);
+    static boolean is_a_bigger_than_b(int a, int b) {
+        if (a > b) {
+            return true;
+        }
+        if (a < b) {
+            return false;
+        }
+        return false;
     }
 }
